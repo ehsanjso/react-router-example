48bc50d19af5b2d0c9354c4dc1eb50d574c80c0b
Ehsan Jahangirzadeh Soure <ehsan.soure@stackadapt.com>
2023-10-12 13:22:58 -0400

    Update header color for main

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <Router>
       <div>
-        <Link to="/">Main page</Link> | 
-        <Link to="/page1">Page 1</Link> | 
+        <Link to="/">Main page</Link> |<Link to="/page1">Page 1</Link> |
         <Link to="/page2">Page 2</Link>
       </div>
 
@@ -18,7 +17,13 @@ function App() {
           <h1>I'm Page 1</h1>
         </Route>
         <Route path="/">
-          <h1>I'm Main page</h1>
+          <h1
+            style={{
+              color: "red",
+            }}
+          >
+            I'm Main page
+          </h1>
         </Route>
       </Switch>
     </Router>
